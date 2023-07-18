23f70259936b125aae3f2575f77799e1437d680b
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2023-07-18 10:37:16 +0530

    upgraded cra

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import HomePage from "./HomePage";
 import Weather from "./Weather";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Route path="/" exact component = {HomePage} />
-        <Route path="/weather" exact component = {Weather} />
-      </div>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/weather" exact element={<Weather />} />
+      </Routes>
     </Router>
   );
 }
